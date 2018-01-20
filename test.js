11e179a7f123c28e58fd17ba3c0811b58e027b4c
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-01-19 22:56:10 -0800

    Optimized AWS SDK imports.

--- test.js
+++ test.js
@@ -4,9 +4,47 @@ var logger = require('the-logger').setup('bunyan', 'TEST', {
 logger.info('Test Start...');
 
 var Promise = require('the-promise');
+var Joi = require('joi');
 
 var client = require('.')('us-east-1');
 client.logger.level = 'info';
+
+
+// return client.Vpc.queryAll('adjasensy')
+
+var dynamo = client.DynamoDB;
+
+var ClusterDeployments = dynamo.define('clusterDeployment', {
+    hashKey: 'accountId',
+    rangeKey: 'full_name',
+
+    timestamps: false,
+
+    schema: {
+        accountId: Joi.string(),
+        full_name: Joi.string(),
+        region: Joi.string(),
+        cluster: Joi.string(),
+        deployment: Joi.string(),
+        isProcessing: Joi.boolean(),
+        processingStartDate: Joi.date(),
+        isDirty: Joi.boolean(),
+        dirtyStartDate: Joi.date()
+    }
+});
+
+ClusterDeployments.update({
+        accountId: 'aaaaa',
+        full_name: 'bbbbb',
+        dirtyStartDate: new Date(),
+        isDirty: true
+    }, {}, function (err, obj) {
+        logger.info('UPDATE RESULT ERR: ', err);
+        logger.info('UPDATE RESULT OBJ: ', obj);
+    });
+
+return;
+
 
 
 return Promise.resolve()
