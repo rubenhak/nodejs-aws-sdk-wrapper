2e8789639bc323bfb8c59ae790fc938d57c56217
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-01-28 12:34:57 -0800

    added logs to repo create

--- test.js
+++ test.js
@@ -6,37 +6,40 @@ logger.info('Test Start...');
 var Promise = require('the-promise');
 var Joi = require('joi');
 
-var client = require('.')('us-east-1');
-client.logger.level = 'info';
-
-var dynamo = client.DynamoDB;
-
-var ClusterDeployments = dynamo.define('clusterDeployment', {
-    hashKey: 'accountId',
-    rangeKey: 'full_name',
-
-    timestamps: false,
-
-    schema: {
-        accountId: Joi.string(),
-        full_name: Joi.string(),
-        region: Joi.string(),
-        cluster: Joi.string(),
-        deployment: Joi.string(),
-        isProcessing: Joi.boolean(),
-        processingStartDate: Joi.date(),
-        isDirty: Joi.boolean(),
-        dirtyStartDate: Joi.date()
-    }
-
-});
-
+var client = require('.')('us-east-1', {
+    profile: 'croundme'
+}, logger);
+client.logger.level = 'verbose';
 //
-return dynamo.model('clusterDeployment').query('723255635421').where('deployment-index').equals('test').exec()
-    .then(result => {
-        return result.Items;
-    })
+// var dynamo = client.DynamoDB;
+//
+// var ClusterDeployments = dynamo.define('clusterDeployment', {
+//     hashKey: 'accountId',
+//     rangeKey: 'full_name',
+//
+//     timestamps: false,
+//
+//     schema: {
+//         accountId: Joi.string(),
+//         full_name: Joi.string(),
+//         region: Joi.string(),
+//         cluster: Joi.string(),
+//         deployment: Joi.string(),
+//         isProcessing: Joi.boolean(),
+//         processingStartDate: Joi.date(),
+//         isDirty: Joi.boolean(),
+//         dirtyStartDate: Joi.date()
+//     }
+//
+// });
+
+// //
+// return dynamo.model('clusterDeployment').query('723255635421').where('deployment-index').equals('test').exec()
+//     .then(result => {
+//         return result.Items;
+//     })
 //return Promise.resolve()
+return client.Repository.create('ruben1')
     .then(obj => {
         logger.info('Result: ', obj);
     })
