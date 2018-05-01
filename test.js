8a2ab38cf9ca291b8a1094a9e8902a79e6c2b5db
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-04-30 20:22:57 -0700

    added throttler

--- test.js
+++ test.js
@@ -14,73 +14,9 @@ var client = require('.')('us-east-1', {
     profile: 'croundme', // 'berlioz'
 }, logger);
 client.logger.level = 'silly';
-//
-// var dynamo = client.DynamoDB;
-//
-// var ClusterDeployments = dynamo.define('clusterDeployment', {
-//     hashKey: 'accountId',
-//     rangeKey: 'full_name',
-//
-//     timestamps: false,
-//
-//     schema: {
-//         accountId: Joi.string(),
-//         full_name: Joi.string(),
-//         region: Joi.string(),
-//         cluster: Joi.string(),
-//         deployment: Joi.string(),
-//         isProcessing: Joi.boolean(),
-//         processingStartDate: Joi.date(),
-//         isDirty: Joi.boolean(),
-//         dirtyStartDate: Joi.date()
-//     }
-//
-// });
-//
-// function doSomething()
-// {
-//     logger.info('doSomething....');
-// }
-//
-// const ThrottledQueue = require('throttled-queue');
-// var throttle = ThrottledQueue(10, 1000, true);
-//
-// function doWork()
-// {
-//     // logger.info('doWork....');
-//     return new Promise(function(resolve, reject) {
-//         throttle(() => {
-//             try {
-//                 return Promise.resolve(doSomething())
-//                     .then(result => {
-//                         resolve(result);
-//                     })
-//                     .catch(reason => {
-//                         reject(reason);
-//                     })
-//             } catch (e) {
-//                 reject(e);
-//             }
-//         });
-//     });
-// }
 
 return Promise.resolve()
     .then(() => client.Vpc.queryAll({}))
-    // .then(() => Promise.serial(_.range(100), () => doWork()))
-    // .then(() => doWork())
-    // .then(() => doWork())
-    // .then(() => doWork())
-    // .then(() => doWork())
-    // .then(() => doWork())
-    // .then(() => doWork())
-    // .then(() => doWork())
-    // .then(() => doWork())
-    // .then(() => doWork())
-    // .then(() => doWork())
-    // .then(() => doWork())
-    // .then(() => doWork())
-    // .then(() => doWork())
     .then(obj => {
         logger.info('Result: ', obj);
     })
