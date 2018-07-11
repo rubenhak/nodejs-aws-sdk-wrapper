6d12a69902af48e4e6d0854d231895c318d1684e
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-07-11 13:01:40 -0700

    Added key and parameter store APIs.

--- test.js
+++ test.js
@@ -18,6 +18,16 @@ client.logger.level = 'verbose';
 return Promise.resolve()
     .then(() => client.DynamoDB)
     .then(() => {})
+    // .then(() => client.SystemsManager.writeParameter('/prod/lab/kaka', '1234-abcd'))
+    // .then(() => client.SystemsManager.queryAllParams('/prod/lab', {WithDecryption :false}))
+    // .then(() => client.KeyManagement.createKey({}, { 'berlioz:deployment': 'prod'}))
+    // .then(() => client.KeyManagement.queryAllKeys({ 'berlioz:deployment': 'prod'}))
+    // .then(() => client.KeyManagement.removeKeyTags('a7d017a1-bd8b-438d-bb03-2b7d23f3b030', ["berlioz:deployment"]))
+    // .then(() => client.KeyManagement.cancelKeyDeletion('fd621cf7-cae3-4f30-b9b3-2143325e75fd'))
+    // .then(() => client.KeyManagement.queryKey('fd621cf7-cae3-4f30-b9b3-2143325e75fd'))
+    // .then(() => client.KeyManagement.queryAllAliases())
+    // .then(() => client.KeyManagement.createAlias("alias/berlioz/kaki", "21440bd1-5b05-42e8-b8ae-25be900a3855"))
+    // .then(() => client.KeyManagement.deleteAlias("alias/berlioz/kaki"))
     .then(obj => {
         logger.info('Result: ', obj);
     })
