f0a79b26b62e9fd6e30ddfabc31691a56b7c15b0
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-09-23 21:08:44 -0700

    method update api

--- test.js
+++ test.js
@@ -15,27 +15,9 @@ var client = require('.')('us-east-1', {
 }, logger);
 client.logger.level = 'verbose';
 
-var helper = client.ApiGatewayHelper;
-
 return Promise.resolve()
     .then(() => client.DynamoDB)
     .then(() => {})
-    .then(() => helper.refresh())
-    .then(() => helper.allMethods)
-
-
-    // .then(() => client.ApiGateway.queryAllRestAPIs('api'))
-    // .then(() => client.ApiGateway.queryAllResources('kzg4eo15y2'))
-    // .then(() => client.ApiGateway.deleteAuthorizer("ef2fcgvofi", "zvmleb"))
-    // .then(() => helper.refresh())
-    // .then(() => helper.allAuthorizers)
-    // .then(() => helper.createAuthorizer("ef2fcgvofi", "mimi", "COGNITO_USER_POOLS", {
-    //     // "authType": "cognito_user_pools",
-    //     "identitySource": "method.request.header.Authorization",
-    //     "providerARNs": [
-    //         "arn:aws:cognito-idp:us-east-1:227920884814:userpool/us-east-1_fDz6tcab0"
-    //     ]
-    // }))
     .then(obj =>  {
         // console.log(obj)
         logger.info('Result: ', obj);
